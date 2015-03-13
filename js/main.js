ae24fee909865e86ff7ea1737ef3e7793675818c
Isabelle6 <isabelle-fortier@hotmail.com>
2015-03-13 18:36:32 -0400

    added tabs with placeholders
    added working tabs and put in placeholder text
    changed the names of classes with “panel” to differentiate the tab
    panels and the nav panels

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 var $nav = $('.nav');
-var $panel = $('.panel');
+var $navPanel = $('.nav-panel');
+
+var $tabs = $('.tabs');
+var $tabPanels = $('.tab-panel');
 
 $nav.on('click', function () {
-    $panel.toggleClass('js-panel-open');
+    $navPanel.toggleClass('js-nav-panel-open');
+});
+
+$tabs.on('click', 'a', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    $tabPanels.filter(':not([hidden])').attr('hidden', true);
+    $(id).removeAttr('hidden');
+    $tabs.find('.js-current').removeClass('js-current');
+    $(this).addClass('js-current');
 });
