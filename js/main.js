812fa3c63c8c7954f6bd4dd3d2b82c4baab1b851
Isabelle6 <isabelle-fortier@hotmail.com>
2015-03-15 18:19:08 -0400

    made buttons work, added transitions
    made the buttons expand and collapse text when clicked

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var $navPanel = $('.nav-panel');
 var $tabs = $('.tabs');
 var $tabPanels = $('.tab-panel');
 
+var $btnDisplay = $('.btn-display-hide');
+var $fishInfo = $('.fish-info');
+
 $nav.on('click', function () {
     $navPanel.toggleClass('js-nav-panel-open');
 });
@@ -15,4 +18,8 @@ $tabs.on('click', 'a', function (e) {
     $(id).removeAttr('hidden');
     $tabs.find('.js-current').removeClass('js-current');
     $(this).addClass('js-current');
+});
+
+$btnDisplay.on('click', function () {
+    $fishInfo.toggleClass('js-fish-info-collapse-expand');
 });
