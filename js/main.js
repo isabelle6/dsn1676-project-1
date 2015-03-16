b3c0422f4b004528508a6d35e22e20b90d6f5f34
Isabelle6 <isabelle-fortier@hotmail.com>
2015-03-16 16:14:07 -0400

    Added fish information

--- js/main.js
+++ js/main.js
@@ -6,10 +6,8 @@ var $tabPanels = $('.tab-panel');
 
 var $btnDisplayBluetang = $('.btn-display-hide-bluetang');
 var $bluetangInfo = $('.bluetang-info');
-
 var $btnDisplayClownfish = $('.btn-display-hide-clownfish');
 var $clownfishInfo = $('.clownfish-info');
-
 var $btnDisplayStarfish = $('.btn-display-hide-starfish');
 var $starfishInfo = $('.starfish-info');
 
@@ -29,11 +27,9 @@ $tabs.on('click', 'a', function (e) {
 $btnDisplayBluetang.on('click', function () {
     $bluetangInfo.toggleClass('js-fish-info-collapse-expand');
 });
-
 $btnDisplayClownfish.on('click', function () {
     $clownfishInfo.toggleClass('js-fish-info-collapse-expand');
 });
-
 $btnDisplayStarfish.on('click', function () {
     $starfishInfo.toggleClass('js-fish-info-collapse-expand');
 });
